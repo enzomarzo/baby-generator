9bfb43523a07a19bffa55294f25aff65370a770b
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-03 18:01:09 +0000

    feat: remove vite defaults

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        
-      </div>
+      <div>it works</div>
     </div>
   )
 }
