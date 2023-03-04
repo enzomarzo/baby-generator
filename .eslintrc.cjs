f0c7ac650f2537c52c4b710b304ca7207ca77cb8
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-04 12:03:38 +0000

    chore: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:react/jsx-runtime",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] }
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "prettier/prettier": "error",
+    "react/require-default-props": 0,
+    "react/button-has-type": 0,
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }]
+  },
+  settings: {
+    alias: {
+      map: [["@", "./src"]]
     }
+  }
 };
