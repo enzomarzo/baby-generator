da36fb369c5657b6803958d56b450eeb2c636e7e
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-06 20:04:53 +0000

    feat: card component enhacement

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -1,12 +1,34 @@
 import classes from "./card.module.scss";
 
-function Button() {
+interface ICardProps {
+  babyName: string;
+  isGirl: boolean;
+}
+
+function Card({ babyName, isGirl }: ICardProps) {
+  const complimentsSynonyms = [
+    "beautiful",
+    "pretty",
+    "lovely",
+    "cute",
+    "wonderful"
+  ];
+  const randomCompliments =
+    complimentsSynonyms[
+      Math.floor(Math.random() * complimentsSynonyms.length)
+    ].toUpperCase();
+
   return (
     <div className={classes.card}>
-      <img className={classes.image} src="baby-girl.png" alt="baby" />
-      <p>baby name</p>
+      <img
+        className={classes.image}
+        src={isGirl ? "baby-girl.png" : "baby-boy.png"}
+        alt="baby"
+      />
+      <p className={classes.cardBabyName}>{babyName.toUpperCase()}</p>
+      <p className={classes.cardComplement}>WHAT A {randomCompliments} NAME!</p>
     </div>
   );
 }
 
-export default Button;
+export default Card;
