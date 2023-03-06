cc371d76d374282947d268a92e34a8620df28708
emarzo <59425374+enzomarzo@users.noreply.github.com>
2023-03-06 20:08:12 +0000

    feat: add dynamic information for into content

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,15 +1,58 @@
+import { useState, useCallback } from "react";
 import Button from "../foundation/button/Button";
 import Card from "../common/card/Card";
 import classes from "./content.module.scss";
+import useBabyNames from "../hooks/useBabyNames";
+import getOrdinal from "../../utils/getOrdinal";
 
 function Content() {
+  const { babyGirls, babyBoys } = useBabyNames();
+  const [babyChoosen, setBabyChoosen] = useState<string[] | null>(null);
+  const [isGirl, setIsGirl] = useState(true);
+
+  const randomBaby = (arr: string[][]) => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  };
+
+  const onClickGirl = useCallback(() => {
+    if (!babyGirls) return;
+    const randomBabyGirl = randomBaby(babyGirls);
+    setBabyChoosen(randomBabyGirl);
+    setIsGirl(true);
+  }, [babyGirls]);
+
+  const onClickBoy = useCallback(() => {
+    if (!babyBoys) return;
+    const randomBabyGirl = randomBaby(babyBoys);
+    setBabyChoosen(randomBabyGirl);
+    setIsGirl(false);
+  }, [babyBoys]);
+
+  if (!babyBoys || !babyGirls) {
+    return null;
+  }
+
+  const nameMessage = (baby: string[]) => {
+    const rankNumber = Number(baby[5]) > 1 ? getOrdinal(baby[5]) : "";
+    return `${baby[3]} is the ${rankNumber} most used name among ${baby[2]} babies`;
+  };
+
   return (
     <div className={classes.cmpNameGenerator}>
+      <h2 className={classes.cmpNameGeneratorTitle}>
+        CHOOSE THE GENDER AND FIND OUT WHAT NAME IS EXPECTING FOR YOUR BABY
+      </h2>
       <div className={classes.cmpNameGeneratorButtonGroup}>
-        <Button>MALE</Button>
-        <Button>FEMALE</Button>
+        <Button onClick={onClickBoy}>BOY</Button>
+        <Button onClick={onClickGirl}>GIRL</Button>
       </div>
-      <Card />
+      {babyChoosen && (
+        <>
+          <Card isGirl={isGirl} babyName={babyChoosen[3]} />
+          <p>{nameMessage(babyChoosen).toUpperCase()}</p>
+        </>
+      )}
     </div>
   );
 }
